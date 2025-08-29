07edd7ebc72ba741b7b884fbdc4dcd7270920d5f
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2025-08-29 15:37:48 +0600

    copy and heart count complete

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ function toIncreaseHeartCount(){
         document.getElementById('heart-icon').innerText=heartCount+1;
 }
 
+//function to increase copy count
+
+function increaseCopyCount()
+ {
+   const heartCount=parseInt(document.getElementById('copy-count').innerText);
+        document.getElementById('copy-count').innerText=heartCount+1;
+ }
+//  copy reuseablity funtion
+
+function toCopyNumber(id)
+{
+   const text = document.getElementById(id).innerText.trim();
+    navigator.clipboard.writeText(text).then(() => {
+        alert("Copied: " + text);
+    });
+}
 document.getElementById('ambulance-heart')
   .addEventListener('click',function()
     {
@@ -34,3 +50,39 @@ document.getElementById('railway-heart')
     {
        toIncreaseHeartCount();
     })
+
+    // copy 
+    document.getElementById("copy-btn-ambulance")
+    .addEventListener("click", function () {
+       toCopyNumber('ambulance-number')
+       increaseCopyCount()
+  });
+document.getElementById("copy-btn-emergency")
+    .addEventListener("click", function () {
+       toCopyNumber('emergency-number')
+       increaseCopyCount()
+  });
+
+  document.getElementById("copy-btn-fire")
+    .addEventListener("click", function () {
+       toCopyNumber('fire-service-number')
+       increaseCopyCount()
+  });
+
+  document.getElementById("copy-btn-police")
+    .addEventListener("click", function () {
+       toCopyNumber('police-number')
+       increaseCopyCount()
+  });
+
+  document.getElementById("copy-btn-heath")
+    .addEventListener("click", function () {
+       toCopyNumber('health-number')
+       increaseCopyCount()
+  });
+
+  document.getElementById("copy-btn-railway")
+    .addEventListener("click", function () {
+       toCopyNumber('railway-number')
+       increaseCopyCount()
+  });
